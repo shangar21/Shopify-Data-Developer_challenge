47182cb558d6a74bb72304d88f14791b91ff4df6
shangar <sharanshangar@gmail.com>
2021-04-30 19:17:38 -0400

    uploads image from URL

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template,  request, g, current_app, flash, redirect, url_for, send_from_directory
+from flask import Flask, render_template,  request, g, current_app, flash, redirect, url_for, send_from_directory, flash
+import urllib.request
 import os
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = os.getcwd() + '/media/'
+
+IMAGE_TYPES = {".png", ".jpeg", ".jpg", ".tiff", ".tif", ".gif", ".bmp", ".ico"}
+
+def save_from_url(url, filename, extention, upload_folder=app.config['UPLOAD_FOLDER']):
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    resource = urllib.request.urlopen(req)
+    output = open(upload_folder+filename+extention,"wb")
+    output.write(resource.read())
+    output.close()
+
 
 @app.route('/')
 def index():
@@ -12,13 +23,25 @@ def index():
 
 @app.route('/upload_images', methods=['GET', 'POST'])
 def upload():
+    global IMAGE_TYPES
+
     if request.method == 'POST':
         files = request.files.getlist("files[]")
+        urls = request.form.to_dict()
+        names = os.listdir(app.config['UPLOAD_FOLDER'])
+        if urls:
+            if urls['url_image_file_name']+urls['image_extention'] in names:
+                flash("{} already in file, use different name".format(urls['url_image_file_name']+urls['image_extention']))
+                return render_template('upload.html', extentions=IMAGE_TYPES)
+            save_from_url(urls['image_url'], urls['url_image_file_name'], urls['image_extention'])
+            
         if files:
             for f in files:
+                if f.filename in names:
+                    flash("{} already in file, overwritten".format(f.filename))
                 f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
             
-    return render_template('upload.html')
+    return render_template('upload.html', extentions=IMAGE_TYPES)
 
 @app.route('/delete_images', methods=['GET', 'POST'])
 def delete():

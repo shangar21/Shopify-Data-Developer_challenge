990da325af8525f59d15478439788bd15cdd4136
shangar <sharanshangar@gmail.com>
2021-04-29 23:49:20 -0400

    deletes single and all files

--- app.py
+++ app.py
@@ -20,6 +20,23 @@ def upload():
             
     return render_template('upload.html')
 
+@app.route('/delete_images', methods=['GET', 'POST'])
+def delete():
+    names = os.listdir(app.config['UPLOAD_FOLDER'])
+    if request.method == 'POST':
+        files = request.form.to_dict()
+        if 'delete_all_imgs' in files:
+            names = os.listdir(app.config['UPLOAD_FOLDER'])
+            for n in names:
+                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], n))
+            return render_template('delete.html', names=[])
+        if files:
+            for f in files:
+                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], f))
+            return render_template('delete.html', names=os.listdir(app.config['UPLOAD_FOLDER']))
+            
+    return render_template('delete.html', names=names)
+
 @app.route('/media/<filename>')
 def media(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
